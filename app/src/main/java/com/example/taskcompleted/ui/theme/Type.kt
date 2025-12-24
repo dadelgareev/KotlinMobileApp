2519e4e62a4b9e8e6484c3fab41d5c25444bd72f
dadelgareev <139441933+dadelgareev@users.noreply.github.com>
2025-12-24 16:08:01 +0500

    таск менеджер

--- app/src/main/java/com/example/taskcompleted/ui/theme/Type.kt
+++ app/src/main/java/com/example/taskcompleted/ui/theme/Type.kt
@@ -14,9 +14,21 @@
  * limitations under the License.
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.0.1" apply false
-    id("com.android.library") version "8.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-}
+package com.example.taskcompleted.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
